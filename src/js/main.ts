44accce00db0ca1f82ac0cd6fe30835604dcb7e5
ted chang <htchang@us.ibm.com>
2019-10-23 02:32:44 -0700

    refactor

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,4 @@
-import * as tf from '@tensorflow/tfjs';
-import WordPieceTokenizer from './tokenization';
+import SentimentAnalysis from './sentimentanalysis';
 
 declare global {
   interface Window {
@@ -9,29 +8,6 @@ declare global {
 
 let statusElement: HTMLLabelElement;
 
-class SentimentAnalysis {
-  private model: tf.GraphModel;
-  private tokenizer: WordPieceTokenizer;
-  constructor(){
-    this.tokenizer = new WordPieceTokenizer(true);
-  }
-
-  async load(){
-    await this.tokenizer.init('http://localhost:3000/vocab.json');
-    this.model = await tf.loadGraphModel('/model/model.json');
-  }
-
-  async analyzeSentence(text: string){
-    return this.analyze(await this.tokenizer.inputFeature(text));
-  }
-
-  analyze(feature: tf.NamedTensorMap){
-    return tf.tidy(() => {
-      let pred: tf.Tensor = this.model.execute({...feature}, 'loss/Softmax') as tf.Tensor;
-      return pred.squeeze([0]);
-    });
-  }
-}
 
 async function pageLoaded() {
   await measureElapsedTime('Model Loading time (ms)', async () => {
@@ -67,7 +43,7 @@ async function loadModel() {
 }
 
 async function predict(text: string) {
-  updateStatus('preidcting');
+  updateStatus('predicting');
   const result = await sa.analyzeSentence(text);
   console.log(JSON.stringify(result.arraySync(), null, 2));
   const res = result.arraySync() as number[];
